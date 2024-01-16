63d7f0d2e336888b4f48cbe1396c134b6248e6c0
Pedro Hernandez de la Rosa <156045284+IDGS-802-19002628@users.noreply.github.com>
2024-01-15 21:33:45 -0600

    Delete tema6-Diccionario.py

--- tema6-Diccionario.py
+++
@@ -1,9 +0,0 @@
-miDiccionario = {"Matricula": 19002628, "Nombre": "Pedro", "Apellido": "Hernandez"}
-
-print(miDiccionario["Nombre"])
-print(miDiccionario)
-print(type(miDiccionario))
-miDiccionario["Nombre"] = "Juan"
-print(miDiccionario)
-miDiccionario["correo"] = "Pedro@gmail.com"
-print(miDiccionario)
